cd599e09e433d493ae550482520938067ace4040
matias <maturod96@gmail.com>
2021-12-20 18:05:54 -0300

    fixes env

--- config/prod.py
+++ config/prod.py
@@ -2,5 +2,5 @@ import os
 
 DEBUG = False
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI= "postgresql://xknvpqiwdzokbx:29b396349f9b58dbb62fb35540b68500070d68622028cc52c6f00767c311691a@ec2-3-217-216-13.compute-1.amazonaws.com:5432/d52mc4olovd0di"
+SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_ENV']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
